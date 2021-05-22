8a529f50e45e75e527db59dd1ef49834c440ce29
canlandazzle <1481086138@qq.com>
2021-05-22 23:11:38 +0800

    制作头像组件

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Avatar url='https://portrait.gitee.com/uploads/avatars/user/1623/4871556_canlan0711_1578976114.png!avatar200' size="50"></Avatar>
+    <Avatar url='https://portrait.gitee.com/uploads/avatars/user/1623/4871556_canlan0711_1578976114.png!avatar200' size="120"></Avatar>
+  </div>
+</template>
+
+<script>
+import Avatar from '@/components/Avatar'
+export default {
+  name: 'App',
+  components: {
+    Avatar,
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="avatar-container">
+    <img class="avatar-img" :src="url" alt="" :style="{
+      width: size + 'px',
+      height: size + 'px'
+    }">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.avatar-img {
+  border-radius: 50%;
+  object-fit: cover;
+  display: block;
+}
+</style>
