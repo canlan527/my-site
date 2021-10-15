ddca3b62f030987829031345d77e9220edf2a973
canlandazzle <1481086138@qq.com>
2021-10-15 23:03:54 +0800

    Empty组件开发完毕

---
+++ src/components/Empty/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="empty-container">
+    <div class="empty-content">
+      <icons type="empty" class="icon"></icons>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icons from "@/components/Icons"
+  export default {
+    name: "Empty",
+    props: {
+      text: {
+        type: String,
+        default: ""
+      }
+    },
+    components: {
+      Icons
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+@import "~@/styles/var";
+
+.empty-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty-content {
+    width: 100%;
+    height: 140px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    .icon {
+      font-size: 100px;
+      color: @lightWords;
+    }
+    p {
+      color: @lightWords;
+      font-size: 26px;
+    }
+  }
+}
+</style>
---
+++ src/components/Empty/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">
+    <empty text="无数据"></empty>
+  </div>
+</template>
+
+<script>
+  import Empty from './'
+  export default {
+    name: 'test',
+    components: {
+      Empty
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
