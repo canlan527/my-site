64119dc9fc5f62b0658bd4819093ea412663cccd
canlandazzle <1481086138@qq.com>
2021-10-17 23:09:51 +0800

    完成concat组件

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="app">
+     <concat :github="github" :email="email" :qq="qq" :weixin="weixin"></concat>
   </div>
 </template>
 
 <script>
+import Concat from '@/components/Concat'
 export default {
   name: 'App',
   data() {
     return {
+      github: 'canlan527',
+      email: 'canlan527@gmail.com',
+      qq: '1481086138',
+      weixin: '是个废物'
     }
   },
   mounted() {
   },
   components: {
+    Concat,
   },
 }
 </script>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 300px;
+  background-color:#333;
+  height: 400px;
+  padding-top:200px;
 }
 </style>
---
+++ src/components/Concat/index.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="concat-container">
+    <ul>
+      <li>
+        <a href="https://github.com/">
+          <icons type="github"></icons>
+          <span>{{ github }}</span>
+        </a>
+      </li>
+      <li>
+        <a>
+          <icons type="mail"></icons>
+          <span>{{ email }}</span>
+        </a>
+      </li>
+      <li>
+        <a href="tencent://message/?Menu=yes&uin=1481086136&Service=300&sigT=45a1e5847943b64c6ff3990f8a9e644d2b31356cb0b4ac6b24663a3c8dd0f8aa12a595b1714f9d45">
+          <icons type="qq"></icons>
+          <span>{{ qq }}</span>
+        </a>
+      </li>
+      <li>
+        <a>
+          <icons type="weixin"></icons>
+          <span>{{ weixin }}</span>
+          <div class="img">
+            <img :src="weixinPic" alt="">
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Icons from "@/components/Icons"
+import weixinPic from "@/assets/images/weixin.jpg"
+
+  export default {
+    name: "Concat",
+    components: {
+      Icons,
+    },
+    props: {
+      github: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      qq: {
+        type: String,
+        required: true,
+      },
+      weixin: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        weixinPic:weixinPic
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+@import "~@/styles/var.less";
+
+.concat-container {
+  width: 100%;
+  background-color: transparent;
+  padding-bottom: 12px;
+  box-sizing: border-box;
+  ul {
+    padding-top: 26px;
+  }
+  li {
+    font-size: 20px;
+    height: 22px;
+    color: @gray;
+    padding-left: 20px;
+    position: relative;
+    &:hover {
+      color: @primary;
+    }
+    &:hover .img {
+      transform: scaleY(1);
+      transition:.3s;
+    }
+    margin-bottom: 22px;
+    .img {
+      width:150px;
+      height: 150px;
+      padding:20px;
+      background-color: #fff;
+      position: absolute;
+      top:-200px;
+      bottom:22px;
+      transform: scaleY(0);
+      transform-origin: center bottom;
+      border-radius: 4px;
+      &:after {
+        content: "";
+        width: 8px;
+        height: 8px;
+        background: #fff;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%) rotate(45deg);
+        bottom:-4px;
+      }
+      img {
+        width:150px;
+        height: 150px;
+      }
+    }
+  }
+
+  .icon-container {
+    margin-right: 8px;
+  }
+}
+</style>
