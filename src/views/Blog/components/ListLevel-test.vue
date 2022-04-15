6a09a18d61f67e2b6935687d2300bf70812da766
灿烂 <1481086138@qq.com>
2022-04-15 23:16:09 +0800

    组件递归

---
+++ src/views/Blog/components/ListLevel-test.vue
@@ -0,0 +1,32 @@
+<template>
+  <list-level :list="list" @select="handleSelct"></list-level>
+</template>
+
+<script>
+import ListLevel from './ListLevel.vue'
+export default {
+  data() {
+    return {
+      list: [
+        {name: '1-1', isActive: false},
+        {name: '2-1', isActive: false},
+        {name: '3-1', isActive: true, children: [
+          {name: '3-1-1', isActive: false},
+          {name: '3-1-2', isActive: false},
+        ]},
+        {name: '4-1', isActive: false}
+      ],
+    }
+  },
+  components: { ListLevel },
+  methods: {
+    handleSelct(item) {
+      console.log(item)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Blog/components/ListLevel.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="list-level-container">
+    <li class="list-level-item" v-for="(item, i) in list" :key="i" >
+      <span @click="handleClick(item)" :class="{'isActive': item.isActive}">{{item.name}}</span>
+      <ListLevel :list="item.children" @select="handleClick" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ListLevel',
+  props: {
+    list: {
+      type: Array,
+    }
+  },
+  methods: {
+    handleClick(item) {
+      this.$emit('select', item)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '~@/styles/var.less';
+
+ .list-level-container {
+   list-style: none;
+   padding:0;
+   font-size:20px;
+   .list-level-container {
+     margin-left: 1em;
+     padding: 6px 0;
+   }
+   .isActive {
+     color:@warn;
+   }
+
+ } 
+</style>
