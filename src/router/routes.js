0de9c660d8a2d46d52eeb80a6d14009d50090974
灿烂 <1481086138@qq.com>
2022-05-30 15:15:28 +0800

    使用nprogress优化项目体验

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,85 @@
+import "nprogress/nprogress.css";
+import { start, done, configure } from "nprogress";
+
+configure({
+  trickleSpeed: 20,
+  showSpinner: false,
+});
+
+// nprograss 进度条
+function delay(duration) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+}
+
+function getPageComponent(pageCompResolver) {
+  return async () => {
+    start();
+    if (process.env.NODE_ENV === "development") {
+      await delay(1000);
+    }
+    const comp = await pageCompResolver();
+    done();
+    return comp;
+  };
+}
+
 // 页面分包： 路由懒加载
-const Home = () => import('@/views/Home'/* webpackChunkName: Home */);
-const About = () => import('@/views/About'/* webpackChunkName: About */);
-const Project = () => import('@/views/Project'/* webpackChunkName: Project */);
-const Message = () => import('@/views/Message'/* webpackChunkName: Message */);
-const Blog = () => import('@/views/Blog'/* webpackChunkName: Blog */);
-const BlogDetail = () => import('@/views/Blog/Detail'/* webpackChunkName: BlogDetail */);
+const Home = getPageComponent(() =>
+  import("@/views/Home" /* webpackChunkName: Home */)
+);
+const About = getPageComponent(() =>
+  import("@/views/About" /* webpackChunkName: About */)
+);
+const Project = getPageComponent(() =>
+  import("@/views/Project" /* webpackChunkName: Project */)
+);
+const Message = getPageComponent(() =>
+  import("@/views/Message" /* webpackChunkName: Message */)
+);
+const Blog = getPageComponent(() =>
+  import("@/views/Blog" /* webpackChunkName: Blog */)
+);
+const BlogDetail = getPageComponent(() =>
+  import("@/views/Blog/Detail" /* webpackChunkName: BlogDetail */)
+);
 
 const routes = [
-  { name: 'Home', path: '/', component: Home,meta: { title: '首页'}}, 
-  { name: 'About', path: '/about', component: About,meta: { title: '关于我'}}, 
-  { name: 'Project', path: '/project', component: Project,meta: { title: '项目&&效果'}}, 
-  { name: 'Message', path: '/message', component: Message,meta: { title: '留言板'}}, 
-  { name: 'Blog', path: '/blog', component: Blog,meta: { title: '文章列表'}}, 
-  { name: 'cateBlog', path: '/blog/cate/:cateId', component: Blog,meta: { title: '文章列表'}}, 
-  { name: 'blogDetail', path: '/blog/:id', component: BlogDetail,meta: { title: '文章详情'}}, 
-]
+  { name: "Home", path: "/", component: Home, meta: { title: "首页" } },
+  {
+    name: "About",
+    path: "/about",
+    component: About,
+    meta: { title: "关于我" },
+  },
+  {
+    name: "Project",
+    path: "/project",
+    component: Project,
+    meta: { title: "项目&&效果" },
+  },
+  {
+    name: "Message",
+    path: "/message",
+    component: Message,
+    meta: { title: "留言板" },
+  },
+  { name: "Blog", path: "/blog", component: Blog, meta: { title: "文章列表" } },
+  {
+    name: "cateBlog",
+    path: "/blog/cate/:cateId",
+    component: Blog,
+    meta: { title: "文章列表" },
+  },
+  {
+    name: "blogDetail",
+    path: "/blog/:id",
+    component: BlogDetail,
+    meta: { title: "文章详情" },
+  },
+];
 
-export default routes
+export default routes;
